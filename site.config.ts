9e9ae1ad8674fe05510cff1589238927f7094aca
zephyrny <112909751+zephyrny@users.noreply.github.com>
2023-01-02 00:04:33 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,15 +2,15 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'Charlie-ebab7b08ec7a42259bd7317a4417174e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  name: 'Next.js Notion itStarter Kit',
+  domain: 'nextjs-notion-starter-kit.transititvebullsh.it',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
