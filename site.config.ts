2933927ddceef925db4f9c8bfc517a2c7151f6f0
zephyrny <112909751+zephyrny@users.noreply.github.com>
2023-01-02 00:09:33 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,17 +9,17 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion itStarter Kit',
-  domain: 'nextjs-notion-starter-kit.transititvebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Charlie Chien-Wan Wei',
+  domain: 'https://www.notion.so/Charlie-ebab7b08ec7a42259bd7317a4417174e',
+  author: 'Nanyan Wu',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Chalie Chien-Wan Wei Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: '',
+  github: '',
+  linkedin: 'Hello world!',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
